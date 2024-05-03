55c99bfe62f9e7e7c082e968a495d5dd7c0b4158
Anthony Maldonado <anthony@clay.cl>
2024-05-02 23:04:53 -0400

    Auth middleware try catch invalid token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,17 +1,34 @@
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../config');
 
+const User = require('../models/User');
+
 function matchToken(req, res, next) {
   const { headers } = req;
   const token = headers['authorization-token'];
+  let id;
 
   if (!token) {
     return res.status(401).json({ 
       auth: false, message: 'No token provided' 
     });
   }
-  const { id } = jwt.verify(token, jwtSecret);
-  req.userId = id;
+
+  try {
+    id = jwt.verify(token, jwtSecret);
+  }
+  catch (error) {
+    return res.status(401).json({ 
+      auth: false, message: 'Invalid token' 
+    });
+  }
+
+  const user = User.findById(id);
+
+  req.user = {
+    _id: id,
+    role: user.role
+  };
   next();
 }
 
