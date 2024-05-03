e384bac8625e0acf99252726706ce8813da13655
Anthony Maldonado <anthony@clay.cl>
2024-05-02 23:02:44 -0400

    Constans

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   jwtSecret: process?.env?.JWT_SECRET || 'secretKeyToken',
-  jwtExpires: process?.env?.JWT_EXPIRES || '24h' 
+  jwtExpires: process?.env?.JWT_EXPIRES || '24h',
+  allowedRoles:  ['admin', 'creator', 'reader' ]
 }
